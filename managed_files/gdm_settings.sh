0337f6ec12a4a0956c5b5844d69e6c9a50d40f84
Chris Geddings <chris.geddings@duke.edu>
2012-08-30 10:35:31 -0400

    updated confusing comment

--- managed_files/gdm_settings.sh
+++ managed_files/gdm_settings.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 function get_gdm_config_status() {
-    # Echo false for what you don't want, true for what you do
+    # Echo false for if it needs to be configured, true if it is how
+    # you want it
     USERDISABLED=`sudo -u gdm gconftool-2 --get /apps/gdm/simple-greeter/disable_user_list`
     if [ $USERDISABLED != "true" ] ; then
         echo "FALSE"
